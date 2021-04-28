68bc0d9f1dceaa3594812853659718d7bfd4871f
Matthew <matthew.d.baker20@gmail.com>
2021-04-28 18:33:45 -0500

    added some seeder data and path to fetch

--- src/main/java/com/movie/reactspringmovie/controller/MovieController.java
+++ src/main/java/com/movie/reactspringmovie/controller/MovieController.java
@@ -1,8 +1,12 @@
 package com.movie.reactspringmovie.controller;
 
+import com.movie.reactspringmovie.model.Movie;
 import com.movie.reactspringmovie.repository.MovieRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies-api/")
@@ -12,6 +16,11 @@ public class MovieController {
 
     public MovieController(MovieRepository movieDao){
         this.movieDao = movieDao;
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAllMovies(){
+        return movieDao.findAll();
     }
 
 }
--- src/seeder.sql
+++ src/seeder.sql
@@ -1,1 +1,3 @@
 use react_movie_db;
+
+insert into movie(genre, rating, title) VALUES ('action', '5', 'Gangster Squad'), ('comdey', '5', 'Coming to America'), ('comedy', '5', 'Trading Places');
